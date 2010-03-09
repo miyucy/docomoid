49f361e29336ef161f351a872238a95d0696d9df
fistfvck <fistfvck@gmail.com>
2010-03-09 17:10:49 +0900

    change xrds

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -6,10 +6,11 @@ class ServerController < ApplicationController
 <?xml version="1.0" encoding="UTF-8"?>
 <xrds:XRDS
     xmlns:xrds="xri://$xrds"
+    xmlns:openid="http://openid.net/xmlns/1.0"
     xmlns="xri://$xrd*($v*2.0)">
   <XRD>
     <Service priority="0">
-      <Type>#{OpenID::OPENID_IDP_2_0_TYPE}</Type>
+      <Type>http://specs.openid.net/auth/2.0/return_to</Type>
       <URI>#{login_url}</URI>
     </Service>
   </XRD>
